be29c882cfce08d257c62ab6e6949faa262dc764
Shahbaz <shahbazakhtar.web@gmail.com>
2025-04-09 11:33:33 +0500

    Landing page link fixed

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,7 +3,6 @@
 import { Button } from '@/components/ui/button';
 import { ArrowRight, CreditCard, Database } from 'lucide-react';
 import { Terminal } from './terminal';
-import { useEffect } from 'react';
 
 export default function HomePage() {
 
@@ -112,7 +111,7 @@ export default function HomePage() {
             </div>
             <div className="mt-8 lg:mt-0 flex justify-center lg:justify-end">
               <a
-                href="https://github.com/nextjs/saas-starter"
+                href="https://github.com/Excelorithm/SaaSLaunchpad"
                 target="_blank"
               >
                 <Button className="bg-white hover:bg-gray-100 text-black border border-gray-200 rounded-full text-xl px-12 py-6 inline-flex items-center justify-center">
